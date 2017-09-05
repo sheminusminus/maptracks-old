02f2245b79b0a6c551a27b7314b9b7f4e67b23f6
Emily Kolar <emkolardev@gmail.com>
2017-09-04 19:38:21 -0500

    use api key from .env

--- api/controllers/map.js
+++ api/controllers/map.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 class MapController {
-
   static async getSnapped(pathValues) {
+    const key = process.env.MAPS_API_KEY;
+
     const response = await axios({
       method: 'GET',
       url: 'https://roads.googleapis.com/v1/snapToRoads',
       params: {
         path: pathValues.join('|'),
-        key: '',
+        key,
       },
     });
   }
-
 }
 
 export default MapController;
